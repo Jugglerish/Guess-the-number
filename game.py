e4702e6eb05b5fc49b07b9aa4caa67a5df576ada
Ishmeet <144150492+Jugglerish@users.noreply.github.com>
2023-10-01 23:33:01 +0530

    game.py

--- game.py
+++ game.py
@@ -7,17 +7,24 @@ def guess_the_number():
     secret_number = random.randint(1, 100)
     attempts = 0
 
-    while True:
+    while attempts < 10:
         try:
             guess = int(input("Take a guess: "))
             attempts += 1
 
-            if guess < secret_number:
+            if guess < secret_number - 10:
                 print("Too low. Try again.")
-            elif guess > secret_number:
+            elif guess > secret_number + 10:
                 print("Too high. Try again.")
-            else:
+            elif abs(guess - secret_number) <= 10:
+                print("You're close!")
+
+            if guess == secret_number:
                 print(f"Congratulations! You guessed the number {secret_number} in {attempts} attempts.")
+                break
+
+            if attempts == 10:
+                print(f"Sorry, you've reached the maximum number of attempts. The secret number was {secret_number}.")
                 break
         except ValueError:
             print("Invalid input. Please enter a number.")
